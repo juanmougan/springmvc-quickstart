329378f779fb4da6c23e87aa3afd28d737a78aa4
Juan Manuel Mougan <juanmougan@gmail.com>
2016-08-01 10:20:19 -0300

    Started adding Spring Data

--- src/main/java/com/github/juanmougan/examples/model/Message.java
+++ src/main/java/com/github/juanmougan/examples/model/Message.java
@@ -1,42 +1,59 @@
 /**
- * 
+ *
  */
 package com.github.juanmougan.examples.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author juanma
- *
  */
+@Entity
 public class Message {
-	
-	private String subject;
-	
-	private String body;
-	
-	public Message() {
-		
-	}
 
-	public Message(String subject, String body) {
-		super();
-		this.subject = subject;
-		this.body = body;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-	public String getSubject() {
-		return subject;
-	}
+    private String subject;
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
+    private String body;
 
-	public String getBody() {
-		return body;
-	}
+    public Message() {
 
-	public void setBody(String body) {
-		this.body = body;
-	}
+    }
+
+    public Message(String subject, String body) {
+        super();
+        this.subject = subject;
+        this.body = body;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 
 }
--- src/main/java/com/github/juanmougan/examples/sevice/impl/MessageServiceImpl.java
+++ src/main/java/com/github/juanmougan/examples/sevice/impl/MessageServiceImpl.java
@@ -6,6 +6,8 @@ package com.github.juanmougan.examples.sevice.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.github.juanmougan.examples.repository.MessageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.github.juanmougan.examples.model.Message;
@@ -18,23 +20,30 @@ import com.github.juanmougan.examples.sevice.MessageService;
 @Service("employeeService")
 public class MessageServiceImpl implements MessageService {
 	
-	private List<Message> messages = new ArrayList<>();
+	@Autowired
+	private MessageRepository messageRepository;
+
+	//private List<Message> messages = new ArrayList<>();
 	
 	public MessageServiceImpl() {
-		this.messages.add(new Message("Hello", "Welcome to the system"));
-		this.messages.add(new Message("Goodbye", "Have a nice day"));
+		//this.messages.add(new Message("Hello", "Welcome to the system"));
+		//this.messages.add(new Message("Goodbye", "Have a nice day"));
+		this.messageRepository.save(new Message("Hello", "Welcome to the system"));
+		this.messageRepository.save(new Message("Goodbye", "Have a nice day"));
 	}
 
 	// TODO do real implementation
 	@Override
 	public List<Message> findAll() {
-		return this.messages;
+		//return this.messages;
+		return (List<Message>) this.messageRepository.findAll();
 	}
 
 	@Override
 	public Message createMessage(Message message) {
-		this.messages.add(message);
-		return message;
+		//this.messages.add(message);
+		//return message;
+		return this.messageRepository.save(message);
 	}
 
 }
