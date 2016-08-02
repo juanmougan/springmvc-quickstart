9564c83808216e5ba8a5cac89fded127326b24f6
Juan Manuel Mougan <juanmougan@gmail.com>
2016-08-02 00:04:07 -0300

    Removed commented code

--- src/main/java/com/github/juanmougan/examples/sevice/impl/MessageServiceImpl.java
+++ src/main/java/com/github/juanmougan/examples/sevice/impl/MessageServiceImpl.java
@@ -23,26 +23,17 @@ public class MessageServiceImpl implements MessageService {
 	@Autowired
 	private MessageRepository messageRepository;
 
-	//private List<Message> messages = new ArrayList<>();
-	
 	public MessageServiceImpl() {
-		//this.messages.add(new Message("Hello", "Welcome to the system"));
-		//this.messages.add(new Message("Goodbye", "Have a nice day"));
-		//this.messageRepository.save(new Message("Hello", "Welcome to the system"));
-		//this.messageRepository.save(new Message("Goodbye", "Have a nice day"));
+
 	}
 
-	// TODO do real implementation
 	@Override
 	public List<Message> findAll() {
-		//return this.messages;
 		return (List<Message>) this.messageRepository.findAll();
 	}
 
 	@Override
 	public Message createMessage(Message message) {
-		//this.messages.add(message);
-		//return message;
 		return this.messageRepository.save(message);
 	}
 
